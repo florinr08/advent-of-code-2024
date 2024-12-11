776341822e2a9f490afcd7cd20d4008de1e0fe96
Florin Radica <florin.radica@gmail.com>
2024-12-11 23:41:20 +0000

    Refactor reading input

--- php/day-1/read-input.php
+++ php/day-1/read-input.php
@@ -1,23 +1,13 @@
 <?php
 
-$test_input = '3   4
-4   3
-2   5
-1   3
-3   9
-3   3
-';
-
-$input = file_get_contents('input.txt');
+$input = file('input.txt', FILE_IGNORE_NEW_LINES);
 
 $left_list = $right_list = [];
 
-foreach (explode("\n", $input) as $line) {
-    if (! empty($line)) {
-        preg_match('/(\d+) +(\d+)/', $line, $matches);
-        $left_list[] = intval($matches[1]);
-        $right_list[] = intval($matches[2]);
-    }
+foreach ($input as $line) {
+    preg_match('/(\d+) +(\d+)/', $line, $matches);
+    $left_list[] = intval($matches[1]);
+    $right_list[] = intval($matches[2]);
 }
 
 return [$left_list, $right_list];
